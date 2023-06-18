29a8a15e68d1aee3ec997a6277bbbfc826a770b4
Joun <urbanscratcher@gmail.com>
2023-06-18 17:32:46 +0900

    DateCounter done - useReducer

---
+++ 10-react-quiz/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header'
+import Main from './Main';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
