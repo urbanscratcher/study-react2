e2abbc8fd1899423fb50146a5ecc539a06880d96
Joun <urbanscratcher@gmail.com>
2023-06-22 05:58:28 +0900

    react hook working

---
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,42 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+
+  const { data, error } = await supabase
+    .from('cabins')
+    .select('*');
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be loaded');
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([newCabin]);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be created');
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase
+    .from('cabins')
+    .delete()
+    .eq('id', id);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be deleted');
+  }
+
+}
+
