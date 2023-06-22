431b35112af75d779c43cb564396881506860f78
Joun <urbanscratcher@gmail.com>
2023-06-23 05:07:56 +0900

    table done

--- 17-the-wild-oasis/src/features/cabins/CabinTable-v1.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable-v1.jsx
@@ -4,7 +4,15 @@ import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
-import Table from "../../ui/Table";
+
+const Table = styled.div`
+  border: 1px solid var(--color-grey-200);
+
+  font-size: 1.4rem;
+  background-color: var(--color-grey-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -26,20 +34,18 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header>
+    <Table role="table">
+      <TableHeader role="row">
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </Table.Header>
-
-      <Table.Body
-        data={cabins}
-        render={(c) => <CabinRow cabin={c} key={c.id} />}
-      />
+      </TableHeader>
+      {cabins.map((c) => (
+        <CabinRow cabin={c} key={c.id} />
+      ))}
     </Table>
   );
 }
