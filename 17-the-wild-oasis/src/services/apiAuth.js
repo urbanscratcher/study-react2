49b769ffb6754a23217d9b84a377641e78aa3c3a
Joun <urbanscratcher@gmail.com>
2023-06-28 23:08:37 +0900

    dashboard stat done

--- 17-the-wild-oasis/src/services/apiAuth.js
+++ 17-the-wild-oasis/src/services/apiAuth.js
@@ -12,7 +12,6 @@ export async function signup({ fullName, email, password }) {
 
   if (error) throw new Error(error.message);
 
-  console.log(data);
   return data;
 }
 
@@ -30,7 +29,6 @@ export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) return null;
   const { data, error } = await supabase.auth.getUser();
-  console.log(data);
 
   if (error) throw new Error(error.message);
   return data?.user;
