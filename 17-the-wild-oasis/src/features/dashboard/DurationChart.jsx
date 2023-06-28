2d0f0d8c1c9104fadc4faa81aeb867bfaaa723a3
Joun <urbanscratcher@gmail.com>
2023-06-29 00:00:13 +0900

    pie chart

--- 17-the-wild-oasis/src/features/dashboard/DurationChart.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DurationChart.jsx
@@ -1,4 +1,14 @@
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
 import styled from "styled-components";
+import Heading from "../../ui/Heading";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -69,22 +79,22 @@ const startDataDark = [
   },
   {
     duration: "2 nights",
-    value: 0,
+    value: 3,
     color: "#c2410c",
   },
   {
     duration: "3 nights",
-    value: 0,
+    value: 5,
     color: "#a16207",
   },
   {
     duration: "4-5 nights",
-    value: 0,
+    value: 4,
     color: "#4d7c0f",
   },
   {
     duration: "6-7 nights",
-    value: 0,
+    value: 7,
     color: "#15803d",
   },
   {
@@ -94,12 +104,12 @@ const startDataDark = [
   },
   {
     duration: "15-21 nights",
-    value: 0,
+    value: 2,
     color: "#1d4ed8",
   },
   {
     duration: "21+ nights",
-    value: 0,
+    value: 1,
     color: "#7e22ce",
   },
 ];
@@ -130,3 +140,48 @@ function prepareData(startData, stays) {
 
   return data;
 }
+
+function DurationChart({ confirmedStays }) {
+  const { isDarkMode } = useDarkMode();
+  const startData = isDarkMode ? startDataDark : startDataLight;
+  const data = prepareData(startData, confirmedStays);
+
+  return (
+    <ChartBox>
+      <Heading as="h2">Stay duration summary</Heading>
+      <ResponsiveContainer width="100%" height={240}>
+        <PieChart>
+          <Pie
+            data={data}
+            nameKey="duration"
+            dataKey="value"
+            innerRadius={85}
+            outerRadius={110}
+            cx="40%"
+            cy="50%"
+            paddingAngle={3}
+          >
+            {data.map((entry) => (
+              <Cell
+                fill={entry.color}
+                stroke={entry.color}
+                key={entry.duration}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend
+            verticalAlign="middle"
+            align="right"
+            width="30%"
+            layout="vertical"
+            iconSize={15}
+            iconType="circle"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+  );
+}
+
+export default DurationChart;
