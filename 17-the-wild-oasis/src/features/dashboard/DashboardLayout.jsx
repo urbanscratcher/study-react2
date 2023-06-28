73bb6e744b277f3a848b2fdf5292696963e6b98a
Joun <urbanscratcher@gmail.com>
2023-06-28 23:47:19 +0900

    chart done

--- 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import useRecentStays from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -23,7 +24,6 @@ function DashboardLayout() {
   const { cabins, isLoading: isLoading3 } = useCabins();
 
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
-  console.log(bookings);
 
   return (
     <StyledDashboardLayout>
@@ -35,8 +35,7 @@ function DashboardLayout() {
       />
       <div>Statistics</div>
       <div>Todays activity</div>
-      <div>Chart stay durations</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
